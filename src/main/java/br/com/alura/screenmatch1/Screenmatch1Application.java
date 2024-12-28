457e2d3d08941276ce36226e86ebbe5013d790c4
Heberth Severo <hvinicius150@gmail.com>
2024-12-28 20:06:13 -0300

    Modelando os dados da aplicacao

--- src/main/java/br/com/alura/screenmatch1/Screenmatch1Application.java
+++ src/main/java/br/com/alura/screenmatch1/Screenmatch1Application.java
@@ -1,11 +1,17 @@
 package br.com.alura.screenmatch1;
 
+import br.com.alura.screenmatch1.model.DadosEpisodio;
 import br.com.alura.screenmatch1.model.DadosSerie;
+import br.com.alura.screenmatch1.model.DadosTemporada;
+import br.com.alura.screenmatch1.principal.Principal;
 import br.com.alura.screenmatch1.service.ConsumoApi;
 import br.com.alura.screenmatch1.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Screenmatch1Application implements CommandLineRunner {
@@ -16,14 +22,7 @@ public class Screenmatch1Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-//		System.out.println(json);
-//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
